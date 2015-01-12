f9996bcc198e10c7f07ef249ba930c3649de955a
ivchenkodima <ivchenkodima@gmail.com>
2015-01-13 00:25:10 +0200

    add TaskServices

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,9 @@
 'use strict';
 
-angular.module('todo.services', []).service('ProjectsService', ProjectsService);
+angular.module('todo.services', [])
+.service('ProjectsService', ProjectsService)
+.service('TaskService', TaskService)
+
 
 function ProjectsService () {
 
@@ -41,4 +44,49 @@ function ProjectsService () {
         window.localStorage['lastActiveProject']  = this.getIndexOfProject(project) || 0;
     };
     this.loadFromStorage();
+}
+
+TaskService.$inject = ["$scope"];
+function TaskService($scope)  {
+  $scope.saveTask = function (task) {
+      if ($scope.getTaskID(task) > -1) $scope.editTask(task);
+      else $scope.addNewTask(task);
+    };
+  $scope.addNewTask = function(task){
+    if(!$scope.activeProject || !task){
+       return;
+    }
+    $scope.activeProject.tasks.push({
+      title: task.title,
+      date: new Date(task.date) || new Date(),
+      checked: false,
+      comment: task.comment
+    });
+
+    $scope.closeNewTask();
+    task.title = null;
+    task.date= new Date();
+    task.comment = null;
+    task.checked = false;
+  };
+  
+  $scope.editTask = function(task){
+    console.log("active" + $scope.activeProject.activeTask.title);
+    if (typeof activeTask === 'undefined') return;
+    if(!$scope.activeProject.activeTask || !activeTask) return;
+    var indexOf = $scope.activeProject.activeTask;
+
+    $scope.activeProject.activeTask.title = task.title || $scope.activeProject.activeTask.title;
+    $scope.activeProject.activeTask.date = task.date || $scope.activeProject.activeTask.date;
+    $scope.activeProject.activeTask.comment = task.comment || $scope.activeProject.activeTask.comment;
+    $scope.activeProject.activeTask.checked = false;
+     
+    $scope.closeEditTask();
+    task.title = null;
+    task.date= new Date();
+    task.comment = null;
+    task.checked = false;
+    
+    $scope.activeProject.activeTask = null;
+  };
 }
